5b6367ba896da99fafe9f4187a2febff5abdce19
dasnary <113150890+dasnary@users.noreply.github.com>
2022-09-08 20:22:08 -0500

    Main

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,9 +1,11 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
-        Vertebrado[] miVertebrados = new Vertebrado[4];
+        Vertebrado[] miVertebrados = new Vertebrado[6];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[2] = new Vertebrado("Tiburón", "Pez");
+        miVertebrados[3] = new Vertebrado("Burro", "Mamimero");
+        miVertebrados[4] = new Vertebrado("Ballena", "Pez");
         
         return miVertebrados;
     }
